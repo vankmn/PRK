cc1a3c5b2d05d72c9155fd2aa1faf30669bc76b4
Kamil Komon <vankmn@gmail.com>
2023-05-04 21:09:12 +0200

    Start project

---
+++ src/main/java/pl/edu/pw/PRK/PrkApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.pw.PRK;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/edu/pw/PRK/PrkApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.pw.PRK;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
