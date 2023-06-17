5f7fc818502ba3239a4f0f584b86b34d37ce24f7
Szymon Jędrzejewski <szym335@gmail.com>
2023-06-17 10:12:18 +0200

    Minikorekty

--- src/main/java/pl/edu/pw/PRK/dao/ScheduleOfMoviesDAO.java
+++ src/main/java/pl/edu/pw/PRK/dao/ScheduleOfMoviesDAO.java
@@ -2,7 +2,6 @@ package pl.edu.pw.PRK.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.edu.pw.PRK.entity.ScheduleOfMovie;
-
 import java.util.List;
 
 public interface ScheduleOfMoviesDAO extends JpaRepository<ScheduleOfMovie,Integer>{
--- src/main/java/pl/edu/pw/PRK/dao/SeatDao.java
+++ src/main/java/pl/edu/pw/PRK/dao/SeatDao.java
@@ -11,9 +11,6 @@ public interface SeatDao extends JpaRepository<Seat, Integer> {
 
 	List<Seat> findAllByOrderByHallIdAsc();
 
-//	@Query("SELECT s FROM Seat s WHERE s.hall.number = ?1")
-//	List<Seat> findSeatAssignedToHall(int hallId);
-
 	@Query("SELECT s FROM Seat s WHERE s.hall = ?1")
 	List<Seat> findSeatAssignedToHall(Hall hall);
 
